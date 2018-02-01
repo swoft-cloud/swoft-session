73d71db4daf9f3dca896e06ea91f6bc3df388d39
huangzhhui <huangzhwork@gmail.com>
2018-02-02 01:00:40 +0800

    Init

---
+++ src/SessionManager.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Swoft\Session;
+
+use Swoft\App;
+use Swoft\Bean\Annotation\Bean;
+use Swoft\Session\Handler\FileSessionHandler;
+use Swoft\Session\Handler\RedisSessionHandler;
+
+/**
+ * @Bean()
+ */
+class SessionManager
+{
+
+    /**
+     * The session handlers
+     *
+     * @var array
+     */
+    protected $handlers = [
+        'file' => FileSessionHandler::class,
+        'redis' => RedisSessionHandler::class
+    ];
+
+    /**
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Create a handler by config
+     *
+     * @return \SessionHandlerInterface
+     * @throws \InvalidArgumentException
+     */
+    public function createHandlerByConfig(): \SessionHandlerInterface
+    {
+        if (!isset($this->config['driver'])) {
+            throw new \InvalidArgumentException('Session driver required');
+        }
+        $handler = $this->getHandler($this->config['driver']);
+        return $handler;
+    }
+
+    /**
+     * Get a handler by name
+     *
+     * @param string $name
+     * @return \SessionHandlerInterface
+     * @throws \InvalidArgumentException
+     */
+    public function getHandler(string $name): \SessionHandlerInterface
+    {
+        $name = strtolower($name);
+        $this->isValidate($name);
+        $class = $this->handlers[$name];
+        return App::getBean($class);
+    }
+
+    /**
+     * @param string $name
+     * @throws \InvalidArgumentException
+     */
+    protected function isValidate(string $name)
+    {
+        if (!array_key_exists($name, $this->handlers)) {
+            throw new \InvalidArgumentException('Invalid session handler');
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param array $config
+     * @return SessionManager
+     */
+    public function setConfig(array $config): self
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+}
