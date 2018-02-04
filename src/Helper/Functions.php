d44f290d09907a8660bb2415f2507b3027bef8f1
huangzhhui <huangzhwork@gmail.com>
2018-02-05 02:23:02 +0800

    add a session() function to get the SessionManager

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -1,1 +1,12 @@
 <?php
+if (! function_exists('session')) {
+    /**
+     * Get SessionManager
+     *
+     * @return \Swoft\Session\SessionManager
+     */
+    function session()
+    {
+        return \Swoft\App::getBean('sessionManager');
+    }
+}
